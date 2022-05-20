87b5c9b211451cb30295b7170ca17d86da3304a5
CodeSonia <sonia_ah@icloud.com>
2022-05-20 09:52:51 +0000

    Creating views and routing for nav links

--- run.py
+++ run.py
@@ -17,6 +17,16 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
 if __name__ == "__main__":
     # Run the app with the below arguments
     app.run(
