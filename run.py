8ea091c2c321b6e9e9bb6afbac3c7ab443e2d4e5
CodeSonia <sonia_ah@icloud.com>
2022-05-20 15:59:39 +0000

    Using POST method in form to retrieve customer data

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import json
 # Imports flask
 # The capital "F" indicates it is a class
 # import render_template from flask
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 # Create an instance of Flask and store it in var app:
@@ -26,8 +26,22 @@ def about():
     return render_template("about.html", page_title="About", company=data)
 
 
-@app.route("/contact")
+@app.route("/about/<member_name>")
+def about_member(member_name):
+    member = {}
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == member_name:
+                member = obj
+    return render_template("member.html", member=member)
+
+
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        print(request.form.get("name"))
+        print(request.form["email"])
     return render_template("contact.html", page_title="Contact")
   
 
